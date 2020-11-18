4c467394b21745c5f634dcf6745099a84a44c341
flendger <kiru.ee@yandex.ru>
2020-11-18 19:20:14 +0300

    added sending files

--- Client/src/main/java/CloudClient.java
+++ Client/src/main/java/CloudClient.java
@@ -3,12 +3,15 @@ import messages.AbstractMessage;
 import messages.Message;
 import messages.MessageUtils;
 import messages.command.CommandMessage;
+import messages.dataTransfer.DataTransferMessage;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 import java.util.Arrays;
 
 public class CloudClient {
@@ -16,7 +19,8 @@ public class CloudClient {
     private final Socket socket = new Socket();
     private DataOutputStream out;
 
-    private final byte[] buf = new byte[2048];
+    private byte[] readBuf;
+    private byte[] writeBuf;
 
 
     public void connect(String host, int port) throws IOException {
@@ -24,44 +28,52 @@ public class CloudClient {
         try {
             out = new DataOutputStream(socket.getOutputStream());
 
-            Thread readThread = new Thread(() -> {
-                try (DataInputStream in = new DataInputStream(socket.getInputStream())){
-                    while (true) {
-                        int cnt = in.read(buf);
-                        Message msgObject = MessageUtils.getMessageFromBytes(Arrays.copyOf(buf, cnt));
-                        String msg = msgObject.toString();
-                        System.out.println(msg);
-
-                        if (msgObject instanceof CommandMessage) {
-                            CommandMessage comMsg = (CommandMessage) msgObject;
-                            switch (comMsg.getCommand()) {
-                                case MSG_PUT_LS:
-                                    if (comMsg.getData().length > 0) {
-                                        FileList fl = (FileList) MessageUtils.BytesToObject(comMsg.getData());
-                                        System.out.println(fl);
-                                    }
-                                    break;
-                            }
-                        }
-                        if (msg.contains("/close")) {
-                            System.out.println("Bye...");
-                            break;
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
+            Thread readThread = new Thread(this::read);
             readThread.setDaemon(true);
             readThread.start();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void read() {
+        try (DataInputStream in = new DataInputStream(socket.getInputStream())){
+            while (true) {
+                int length = in.readInt();
+                if (length == 0) continue;
+
+                readBuf = Arrays.copyOf(ByteBuffer.allocate(4).putInt(length).array(), length);
+                in.read(readBuf, 4, length - 4);
+                Message msgObject = MessageUtils.getMessageFromBytes(readBuf);
+                String msg = msgObject.toString();
+                System.out.println(msg);
+
+                if (msgObject instanceof CommandMessage) {
+                    CommandMessage comMsg = (CommandMessage) msgObject;
+                    switch (comMsg.getCommand()) {
+                        case MSG_PUT_LS:
+                            if (comMsg.getData().length > 0) {
+                                FileList fl = (FileList) MessageUtils.BytesToObject(comMsg.getData());
+                                System.out.println(fl);
+                            }
+                            break;
+                    }
+                } else if (msgObject instanceof DataTransferMessage) {
+                    System.out.println("Received file...");
+                }
+                if (msg.contains("/close")) {
+                    System.out.println("Bye...");
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -74,6 +86,38 @@ public class CloudClient {
             out.write(bytes);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    //TODO: move sending to FileUtils
+    //TODO: add collecting file form messages
+    public void sendFile(String filePath) throws IOException{
+        Path path = Path.of(filePath);
+        if (! Files.exists(path)) throw new NoSuchFileException("File doesn't exist: " + path.toString());
+        if (Files.isDirectory(path)) throw new NoSuchFileException("File is directory: " + path.toString());
+
+        File file = new File(filePath);
+        FileInputStream fileBytes = new FileInputStream(file);
+        writeBuf = new byte[DataTransferMessage.MAX_DATA_BUFFER_SIZE];
+
+        int fileId = DataTransferMessage.generateFileId();
+
+        int cnt = fileBytes.read(writeBuf);
+        boolean isFirst = true;
+        while (cnt != -1) {
+            DataTransferMessage msg = new DataTransferMessage();
+            msg.setFileId(fileId);
+            msg.setFileName(path.getFileName().toString());
+            msg.setData(writeBuf, cnt);
+            msg.setFirst(isFirst);
+
+            cnt = fileBytes.read(writeBuf);
+            if (cnt == -1) {
+                msg.setEOF(true);
+            }
+            isFirst = false;
+
+            send(msg);
         }
     }
 
--- Core/src/main/java/messages/dataTransfer/DataTransferMessage.java
+++ Core/src/main/java/messages/dataTransfer/DataTransferMessage.java
@@ -4,15 +4,39 @@ import messages.AbstractMessage;
 import messages.MessageType;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+
+
+/**
+ * Message structure:
+ *  msg length - int
+ *  msg type id - short
+ *  msg id - int
+ *  isFirst - byte
+ *  isEOF - byte
+ *  fileId - int
+ *  fileName length - int
+ *  --- end of header ---
+ *  fileName - String (byte[])
+ *  data - byte[]
+ */
 
 public class DataTransferMessage extends AbstractMessage {
 
-    public static final int HEADER_LENGTH = 11;
-    private final ByteBuffer bf = ByteBuffer.allocate(2048);
+    public static final int HEADER_LENGTH = 20;
+    public static final int MAX_DATA_BUFFER_SIZE = 256;
+    private final ByteBuffer bf = ByteBuffer.allocate(HEADER_LENGTH + MAX_DATA_BUFFER_SIZE + 1024); //1024 - for fileName
 
+    private int fileId;
+    private String fileName;
     private byte[] data;
+    private boolean isFirst;
     private boolean isEOF;
+
+    public static int generateFileId() {
+        return (int) (Math.random() * 100000);
+    }
 
     public DataTransferMessage() {
         super(MessageType.DATA_TRANSFER);
@@ -23,6 +47,46 @@ public class DataTransferMessage extends AbstractMessage {
         fromBytes(bytes);
     }
 
+    public int getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(int fileId) {
+        this.fileId = fileId;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data, int len) {
+        this.data = Arrays.copyOf(data, len);
+    }
+
+    public boolean isFirst() {
+        return isFirst;
+    }
+
+    public void setFirst(boolean first) {
+        isFirst = first;
+    }
+
+    public boolean isEOF() {
+        return isEOF;
+    }
+
+    public void setEOF(boolean EOF) {
+        isEOF = EOF;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
     @Override
     public byte[] toBytes() {
         bf.clear();
@@ -30,6 +94,14 @@ public class DataTransferMessage extends AbstractMessage {
         bf.putInt(getLength());
         bf.putShort(msgType.getId());
         bf.putInt(msgId);
+        bf.put((byte) (isFirst ? 1 : 0));
+        bf.put((byte) (isEOF ? 1 : 0));
+        bf.putInt(fileId);
+
+        byte[] nameBytes = fileName.getBytes();
+        bf.putInt(nameBytes.length);
+        bf.put(nameBytes);
+
         bf.put(data);
 
         bf.flip();
@@ -46,18 +118,39 @@ public class DataTransferMessage extends AbstractMessage {
         bf.getShort(); //message type
         msgId = bf.getInt();
 
-        bf.put(isEOF ? (byte)1 : 0);
+        isFirst = bf.get() == (byte) 1;
+        isEOF = bf.get() == (byte) 1;
+        fileId = bf.getInt();
 
-        data = new byte[(int) (length - HEADER_LENGTH)];
+        int nameLength = bf.getInt();
+        byte[] nameBytes = new byte[nameLength];
         int pos = 0;
-        while (bf.hasRemaining()) {
-            data[pos] = bf.get();
+        while (bf.hasRemaining() && pos < nameLength) {
+            nameBytes[pos] = bf.get();
             pos++;
         }
+        fileName = new String(nameBytes, StandardCharsets.UTF_8);
+
+        data = new byte[(int) (length - HEADER_LENGTH - nameLength)];
+        if (bf.hasRemaining()) bf.get(data);
     }
 
     @Override
     public int getLength() {
-        return HEADER_LENGTH + data.length;
+        return HEADER_LENGTH + fileName.getBytes().length + data.length;
+    }
+
+    @Override
+    public String toString() {
+        return "DataTransferMessage{" +
+                "msgId=" + msgId +
+                ", msgLength=" + getLength() +
+                ", msgType=" + msgType +
+                ", fileID=" + fileId +
+                ", fileName=" + fileName +
+                ", isFirst=" + isFirst +
+                ", isEOF=" + isEOF +
+                ", data length='" + data.length + '\'' +
+                '}';
     }
 }
