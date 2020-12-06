0cafac91751dfec55a02fbbd3af32db7bc6a23b4
flendger <kiru.ee@yandex.ru>
2020-12-06 09:16:42 +0300

    restore AuthController

---
+++ client/src/main/java/fxgui/AuthController.java
@@ -0,0 +1,42 @@
+package fxgui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import messages.auth.AuthMessage;
+
+
+public class AuthController {
+
+    private AuthMessage msg;
+
+    @FXML
+    VBox auth;
+
+    @FXML
+    TextField userField;
+
+    @FXML
+    PasswordField passField;
+
+    public void connectBtnAction(ActionEvent actionEvent) {
+        msg = new AuthMessage(userField.getText(), passField.getText(), false);
+        close();
+    }
+
+    public void registerBtnAction(ActionEvent actionEvent) {
+        msg = new AuthMessage(userField.getText(), passField.getText(), true);
+        close();
+    }
+
+    public AuthMessage getMsg() {
+        return msg;
+    }
+
+    private void close() {
+        ((Stage) auth.getScene().getWindow()).close();
+    }
+}
